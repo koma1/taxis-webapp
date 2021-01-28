c04fc78a9fba7b9551047f8db9fe992591bc8817
Andrey Komarov (k0m@) <andrey@komarov.pw>
2021-01-28 13:01:34 +0300

    warnings removed

--- src/main/java/pw/komarov/taxi/security/UserDetailsServiceImpl.java
+++ src/main/java/pw/komarov/taxi/security/UserDetailsServiceImpl.java
@@ -4,13 +4,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import pw.komarov.taxi.persistence.entity.UserEntity;
-
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
 		return null;
 //		UserEntity user = new UserDao();
 //		
