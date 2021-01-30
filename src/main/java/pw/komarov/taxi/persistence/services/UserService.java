7ca4ac14dff56bf718e240dc08d6df58459d2731
Andrey Komarov (k0m@) <andrey@komarov.pw>
2021-01-30 11:09:26 +0300

    Fixed to new AbstractDaoService

--- src/main/java/pw/komarov/taxi/persistence/services/UserService.java
+++ src/main/java/pw/komarov/taxi/persistence/services/UserService.java
@@ -1,31 +1,12 @@
 package pw.komarov.taxi.persistence.services;
 
-import java.util.List;
-
 import pw.komarov.taxi.persistence.dao.UserDao;
 import pw.komarov.taxi.persistence.entity.UserEntity;
-import pw.komarov.taxi.persistence.exceptions.EntityObjectException;
 
-public class UserService implements EntityService<UserEntity> {
+public class UserService extends AbstractDaoService<UserEntity> {
 	private final static UserDao dao = new UserDao();
 	
-	@Override
-	public UserEntity getEntity(Integer id) {
-		return dao.findById(id);
-	}
-
-	@Override
-	public List<UserEntity> getAllEntities() {
-		return dao.getAll();
-	}
-
-	@Override
-	public void save(UserEntity entity) throws EntityObjectException {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void delete(Integer id) throws EntityObjectException {
-		throw new UnsupportedOperationException();
+	public UserService() {
+		super(dao);
 	}
 }
